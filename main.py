7bc78dabdd792bb8eb71dba4c074dc40bf5b58e7
Caliban0101 <rcolman@gmail.com>
2023-04-16 16:41:51 +1000

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,9 @@ model = Model("vosk-model-small-en-us-0.15")
 # Mimic3 server URL
 mimic3_server_url = "http://0.0.0.0:59125"
 
+audio_queue = Queue()
+
+
 # Function to listen for activation word
 def listen_for_activation_word():
     recognizer = KaldiRecognizer(model, 48000)
@@ -65,7 +68,9 @@ def listen_for_activation_word():
     stream.stop_stream()
     stream.close()
 
+
 # Function to transcribe speech to text
+
 def transcribe_speech():
     recognizer = KaldiRecognizer(model, 48000)
     stream = p.open(rate=48000, channels=1, format=pyaudio.paInt16, input=True, frames_per_buffer=1024)
@@ -82,69 +87,6 @@ def transcribe_speech():
     stream.stop_stream()
     stream.close()
 
-# Function to use ChatCompletion and get response
-conversation_history = []
-
-
-def get_response(text):
-    global conversation_history
-
-    with open("sysmsg.txt", "r") as file:
-        system_message = file.read().strip()
-
-    if not conversation_history:
-        conversation_history.append({"role": "system", "content": system_message})
-
-    conversation_history.append({"role": "user", "content": text})
-
-    while sum(len(msg["content"]) for msg in conversation_history) > 3000:
-        conversation_history.pop(0)
-
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=conversation_history
-    )
-
-    reply = response.choices[0].message['content'].strip()
-    conversation_history.append({"role": "assistant", "content": reply})
-
-    return reply
-
-
-audio_queue = Queue()
-
-async def play_response(text):
-    voice = "fr_FR/m-ailabs_low#gilles_g_le_blanc"
-
-    # Split the input text into individual lines
-    sentences = text.split(".")
-
-    # Start the player thread
-    player_thread = threading.Thread(target=audio_player)
-    player_thread.start()
-
-    # Process each sentence
-    for sentence in sentences:
-        # Send a request to the Mimic3 server
-        response = requests.post(
-            f"{mimic3_server_url}/api/tts?voice={voice}",
-            data=sentence.encode(),
-            headers={"Content-Type": "text/plain"},
-        )
-        print("sent")
-        if response.status_code == 200:
-            wav_data = BytesIO(response.content)
-
-            # Pass the wav_data directly to AudioSegment.from_file
-            audio_segment = AudioSegment.from_file(wav_data, format="wav")
-
-            audio_queue.put(audio_segment)
-        else:
-            print(f"Error: Mimic3 server returned status code {response.status_code}")
-
-    # Stop the player thread after all audio segments have been processed
-    audio_queue.put(None)
-    player_thread.join()
 
 def audio_player():
     current_audio = None
@@ -162,14 +104,86 @@ def audio_player():
         play(current_audio)
         current_audio = None  # Reset current_audio for the next playback cycle
 
+
+# Function to use ChatCompletion and get response
+conversation_history = []
+
+
+async def get_response(text):
+    global conversation_history
+
+    with open("sysmsg.txt", "r") as file:
+        system_message = file.read().strip()
+
+    if not conversation_history:
+        conversation_history.append({"role": "system", "content": system_message})
+
+    conversation_history.append({"role": "user", "content": text})
+
+    while sum(len(msg["content"]) for msg in conversation_history) > 3000:
+        conversation_history.pop(0)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=conversation_history,
+        stream=True
+    )
+
+    partial_sentence = ""
+    for chunk in response:
+        delta = chunk.get("choices")[0].get("delta", {})
+        content = delta.get("content")
+        if content is not None:
+            partial_sentence += content.strip()
+
+            if '.' in partial_sentence:
+                sentences = partial_sentence.split('.')
+                for sentence in sentences[:-1]:
+                    yield sentence.strip() + '.'
+
+                partial_sentence = sentences[-1]
+
+    if partial_sentence:
+        yield partial_sentence.strip()
+
+
+async def play_response(sentence_generator):
+    voice = "en_US/vctk_low#p238"
+
+    # Start the player thread
+    player_thread = threading.Thread(target=audio_player)
+    player_thread.start()
+
+    # Process each sentence
+    async for sentence in sentence_generator:
+        response = requests.post(
+            f"{mimic3_server_url}/api/tts?voice={voice}",
+            data=sentence.encode(),
+            headers={"Content-Type": "text/plain"},
+        )
+        print("sent")
+        if response.status_code == 200:
+            wav_data = BytesIO(response.content)
+
+            audio_segment = AudioSegment.from_file(wav_data, format="wav")
+            audio_queue.put(audio_segment)
+        else:
+            print(f"Error: Mimic3 server returned status code {response.status_code}")
+
+    # Stop the player thread after all audio segments have been processed
+    audio_queue.put(None)
+    player_thread.join()
+
+
 # Main loop
 while True:
     if listen_for_activation_word():
         question = transcribe_speech()
         if question:
             print("You asked:", question)
-            response_text = get_response(question)
+            sentence_generator = get_response(question)
+            response_text = " ".join([sentence async for sentence in sentence_generator])
             print("Assistant:", response_text)
-            asyncio.run(play_response(response_text))
+            asyncio.run(play_response(sentence_generator))
         else:
             print("Could not understand your question")
